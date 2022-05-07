1aed4495c597fc2a952125398d2641c58cd7b80f
Mestan Ünsal TEKİŞ <83555018+tekis90@users.noreply.github.com>
2022-05-07 20:13:53 +0300

    Update dht11.ino

--- dht11.ino
+++ dht11.ino
@@ -7,15 +7,14 @@
 #include "FirebaseESP8266.h"
 
 
-//1. Firebase veritabanı adresini, Token bilgisini ve ağ adresi bilgilerinizi giriniz.
-#define FIREBASE_HOST "dht11klimakontrol-default-rtdb.firebaseio.com" // http:// veya https:// olmadan yazın
-#define FIREBASE_AUTH "aYkxqLkDUHPF5aXkP6eQO7tf3IVe7aFIZ6huetoh"
-#define WIFI_SSID "TEKIS"
-#define WIFI_PASSWORD "Tekis2022Mestan"
+
+#define FIREBASE_HOST "***************" 
+#define FIREBASE_AUTH "************"
+#define WIFI_SSID "***********"
+#define WIFI_PASSWORD "*********"
 String gelen;
 
 
-//2. veritabanim adında bir firebase veritabanı nesnesi oluşturuyoruz
 FirebaseData Data;
 
 dht11 DHT11;
@@ -24,14 +23,7 @@ void DHT11_Okuma()
 {
   Serial.println("\n");
   int chk = DHT11.read(DHT11PIN);
-  /*
-    Serial.print("Nem (%): ");
-    Serial.println((float)DHT11.humidity, 2);
 
-    Serial.print("Sicaklik (Celcius): ");
-    Serial.println((float)DHT11.temperature, 2);
-
-    delay(2000);*/
 
 }
 
@@ -52,11 +44,11 @@ void setup()
   Serial.println();
 
 
-  //3. Firebase bağlantısı başlatılıyor
+  
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-  //4. Ağ bağlantısı kesilirse tekrar bağlanmasına izin veriyoruz
+  
   Firebase.reconnectWiFi(true);
   pinMode(role, OUTPUT);
   digitalWrite(role, LOW);
@@ -76,11 +68,9 @@ void loop()
   Serial.println(gelen);
 
 
-  if (Firebase.getString(Data, "/kontrol")) //Alınacak veri tipine göre getInt, getBool, getFloat, getDouble, getString olarak kullanılabilir.
+  if (Firebase.getString(Data, "/kontrol")) 
   {
-    //bağlantı başarılı ve veri geliyor ise
-    //Serial.print("String tipinde veri alımı başarılı, veri = ");
-    //Serial.println(veritabanim.stringData());
+    
     if (Data.stringData() == "1") {
       digitalWrite(role, HIGH);
       Serial.println("Klima Açık");
